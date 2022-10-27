eeafca1507234019dd5fc8d02a7cc4728d805819
The-TT-Hacker <timthacker97@gmail.com>
2022-10-27 18:25:39 +1100

    vulnerable python

---
+++ vulnerable.py
@@ -0,0 +1,10 @@
+
+
+from django.db.models.expressions import RawSQL
+from django.contrib.auth.models import User
+
+# secure example
+User.objects.annotate(val=RawSQL('secure', []))
+
+# insecure example
+User.objects.annotate(val=RawSQL('%secure' % 'nos', []))
