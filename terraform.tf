6db9f61cb9e1985340289b8ec777fa1719eb0df2
The-TT-Hacker <timthacker97@gmail.com>
2022-11-02 00:27:19 +1100

    vuln terraform

---
+++ terraform.tf
@@ -0,0 +1,25 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "<= 4.25.0"
+    }
+  }
+}
+
+provider "aws" {
+  default_tags {
+    tags = {
+      Deployment = var.deployment_id
+    }
+  }
+}
+
+resource "aws_ecr_repository" "bad_example" {
+    name                 = "bar"
+    image_tag_mutability = "MUTABLE"
+
+    image_scanning_configuration {
+        scan_on_push = false
+    }
+}
